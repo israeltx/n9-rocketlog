a2dfb09fd48cbbf053b35d68cfa3566f3bf7338c
Israel Teixeira <israeltx@gmail.com>
2025-07-30 17:01:17 -0300

    Add authorization middleware

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,10 +1,4 @@
 import { Request, Response } from "express";
-// import { AppError } from "@/utils/AppError";
-// import { authConfig } from "@/configs/auth";
-// import { prisma } from "@/database/prisma";
-// import { sign } from "jsonwebtoken"
-// import { compare } from "bcrypt";
-// import { z } from "zod"
 
 class DeliveriesController {
   create(request: Request, response: Response) {
