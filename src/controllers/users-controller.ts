5af68443f8a60bada0b7812d68232d3062642089
Israel Teixeira <israeltx@gmail.com>
2025-07-30 14:58:39 -0300

    Add validation and logic for new users

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
 import { hash } from "bcrypt";
 import { z } from "zod"
 
@@ -12,9 +14,25 @@ class UsersController {
 
     const { name, email, password } = bodySchema.parse(request.body)
 
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email } })
+
+    if (userWithSameEmail) {
+      throw new AppError('User with the same email already exists')
+    }
+
     const hashedPassword = await hash(password, 8)
 
-    return response.json({ message: 'ok', hashedPassword })
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    })
+
+    const { password: _, ...userWithoutPassord } = user
+
+    return response.json(userWithoutPassord)
   }
 }
 
