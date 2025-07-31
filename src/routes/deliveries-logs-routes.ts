703dcd90731d7927c37a57be071f8e825fbad777
Israel Teixeira <israeltx@gmail.com>
2025-07-31 16:06:28 -0300

    Show delivery details

--- src/routes/deliveries-logs-routes.ts
+++ src/routes/deliveries-logs-routes.ts
@@ -7,5 +7,6 @@ const deliveriesLogsRoutes = Router()
 const deliveriesLogsController = new DeliveriesLogsController()
 
 deliveriesLogsRoutes.post('/', ensureAuthenticated, verifyUserAuthorization(['sale']), deliveriesLogsController.create)
+deliveriesLogsRoutes.get('/:delivery_id/show', ensureAuthenticated, verifyUserAuthorization(['sale', 'customer']), deliveriesLogsController.show)
 
 export { deliveriesLogsRoutes }
